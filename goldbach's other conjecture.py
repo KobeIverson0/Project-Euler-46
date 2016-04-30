6eb4b879b5ab8870c7dc6e029c597e203c3fe4ff
KobeIverson0 <KobeIverson0@users.noreply.github.com>
2016-04-30 13:58:00 +0800

    Added files via upload

---
+++ goldbach's other conjecture.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# coding:utf-8
+
+from math import sqrt
+
+def is_Prime(n):
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+judge = True
+num = 33
+
+while judge:
+    judge = False
+    if not is_Prime(num):
+        for i in range(1, int(sqrt(num / 2)) + 1):
+            if is_Prime(num - 2 * i ** 2):
+                num += 2
+                judge = True
+                break
+    else:
+        num += 2
+        judge = True
+
+print num
